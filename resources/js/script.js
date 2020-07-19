ea2b73580ed3583bc815b9141468a2ad16455974
Don Jose Mathew <59858506+dontech09@users.noreply.github.com>
2020-07-19 20:21:07 +0530

    Girlscript Bilaspur Hackathon Final Submission

--- resources/js/script.js
+++ resources/js/script.js
@@ -29,9 +29,15 @@ $(document).ready(function() {
     $('.js--scroll-to-plans').click(function () {
        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
     });
+    $('.js--scroll-to-team').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-team').offset().top}, 1000); 
+    });
     
     $('.js--scroll-to-start').click(function () {
        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    $('.js--scroll-to-food').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-food').offset().top}, 1000); 
     });
     
     
